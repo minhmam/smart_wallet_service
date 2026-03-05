1867f5288d4f8083ad73c2f41a5468af7dee46e9
minhpt <minhpt@unikom.vn>
2026-03-05 08:56:57 +0700

    init

---
+++ src/main/java/com/minhpt/smart_wallet_service/SmartWalletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.minhpt.smart_wallet_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWalletServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWalletServiceApplication.class, args);
+	}
+
+}
